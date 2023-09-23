5c6063ee5cbde76ec8791aed52d16b8f53ec91f6
Roki Miftah Kamaludin <rokimiftah@gmail.com>
2023-09-23 07:16:21 +0700

    include skrolgram urls to core urls

--- _core/urls.py
+++ _core/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("", include("APP.urls")),
+    path("", include("skrolgram.urls")),
     # path("__reload__/", include("django_browser_reload.urls")),
 ]
